4e167f1398cbec66455520787ac48d96f86ceed4
Mike Burkle <Michael.burkle@lvlsvn.com>
2014-10-18 00:31:29 -0400

    added shit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  resources :games
+  resources :games do
+    resources :game_attributes
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

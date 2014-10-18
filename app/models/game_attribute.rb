3fbf5b8a96e1cc75c4e7d0c3ae90624e17afbb32
Josh Schramm <josh.schramm@gmail.com>
2014-10-17 23:50:16 -0400

    setup acts as tree

--- app/models/game_attribute.rb
+++ app/models/game_attribute.rb
@@ -1,3 +1,7 @@
 class GameAttribute < ActiveRecord::Base
+  acts_as_tree order: :sequence
 
+  validates :sequence, uniqueness: { scope: [:parent_id] }
+  validates :sequence, presence: true
+  validates :name, presence: true
 end

0461848827513fad7cf3c2838b64086c979f173a
Josh Schramm <josh.schramm@gmail.com>
2014-10-18 01:23:11 -0400

    Added sendgrid addon and enabled some display of attributes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.initalize_on_precompile = false
+  config.action_mailer.default_url_options = { host: 'evening-temple-6139.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
